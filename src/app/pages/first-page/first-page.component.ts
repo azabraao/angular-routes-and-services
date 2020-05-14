37a99d82176a98d6ed3fee8e6d60d4523d914848
azabraao <az.abraao@gmail.com>
2020-05-14 00:38:09 -0300

    Lista RM dos integrantes do grupo Cative

--- src/app/pages/first-page/first-page.component.ts
+++ src/app/pages/first-page/first-page.component.ts
@@ -7,6 +7,34 @@ import { Component, OnInit } from '@angular/core';
 })
 export class FirstPageComponent implements OnInit {
 
+  integrantes: any[] = [
+    {
+      nome: "Abraão Azevedo",
+      RM: 83983
+    },
+
+    {
+      nome: "Geovanne Amorim",
+      RM: 82578
+    },
+    {
+      nome: "Iago Garcia",
+      RM: 82448
+    },
+    {
+      nome: "Luis Guilherme",
+      RM: 80389
+    },
+    {
+      nome: "Nicolas Nunes",
+      RM: 82331
+    },
+    {
+      nome: "Rodrigo Tanaka",
+      RM: 83888
+    },
+  ]
+
   constructor() { }
 
   ngOnInit() {
